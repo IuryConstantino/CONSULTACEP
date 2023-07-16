5df2ec7bf78391d8e8bc5bff2e5b9c932bf9a11e
IuryConstantino <121960144+IuryConstantino@users.noreply.github.com>
2023-07-16 12:51:59 -0300

    Update main.py

--- venv/main.py
+++ venv/main.py
@@ -1,3 +1,23 @@
 #Consulta CEP:
 
 import requests
+
+
+def buscar_cep(cep):
+    url = f"https://viacep.com.br/ws/{cep}/json/"
+    response = requests.get(url)
+    data = response.json()
+    return data
+
+cep = input("Digite o CEP: ")
+endereco = buscar_cep(cep)
+
+if "erro" not in endereco:
+    print("CEP:", endereco["cep"])
+    print("Logradouro:", endereco["logradouro"])
+    print("Complemento:", endereco["complemento"])
+    print("Bairro:", endereco["bairro"])
+    print("Cidade:", endereco["localidade"])
+    print("Estado:", endereco["uf"])
+else:
+    print("CEP não encontrado.")
